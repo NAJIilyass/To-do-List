7617cf0f178349eb0ef8e9e915763dd72982b6a6
IlYess <najiilyassoo@gmail.com>
2023-12-19 21:04:59 +0100

    Initialise Backend Architecture

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const express = require('express')
+/*const tasksRoutes = require('./routes/tasks')
+const userRoutes = require('./routes/users')
+const mongoose = require('mongoose');
+const cors = require('cors');*/
+
+//Express app
+const app = express()
+
+/*
+//connnect to MongoDB
+mongoose.connect(process.env.MONG_URI)
+    .then((result) => app.listen(process.env.PORT, () => {
+        console.log('Connected to DB & Listening on port '+ process.env.PORT)
+    }))
+    .catch((err) => console.log(err))
+
+//Middleware
+app.use(express.json())
+*/
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next();
+})
+/*
+app.use(cors())
+
+
+//Routes
+app.use('/api/tasks', tasksRoutes)
+app.use('/api/users', userRoutes)*/
