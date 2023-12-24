e93a0094faddadae524a882b7c642c90be7a611b
IlYess <najiilyassoo@gmail.com>
2023-12-24 15:42:48 +0100

    Finishing the taskController  : CRUD methods

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,4 +1,4 @@
-/*const Task = require('../models/taskModel')
+const Task = require('../models/taskModel')
 const mongoose = require('mongoose')
 
 //GET all tasks
@@ -85,4 +85,4 @@ module.exports = {
     createTask,
     deleteTask,
     updateTask
-}*/
+}
