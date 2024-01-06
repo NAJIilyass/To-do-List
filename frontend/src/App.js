8aff1c1f415449a0900bd4a49bd37a6ea07342bb
IlYess <najiilyassoo@gmail.com>
2024-01-06 18:54:55 +0100

    Setup routing pages : Home, Signup & Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,37 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+
+//Pages & Components
+import Home from './pages/Home'
+//import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { useAuthContext } from './hooks/useAuthContext';
+
 function App() {
+  const { user } = useAuthContext()
+  
   return (
-    <div className="App">
-      
+    <div className="bg-blue-950 min-h-screen font-mono min-w-fit">
+      <BrowserRouter>
+        {/* <Navbar/> */}
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={user ? <Home /> : <Navigate to="/login" />} 
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />} 
+            />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />} 
+            />
+          </Routes>
+        </div>
+        <footer className="text-transparent">.</footer>
+      </BrowserRouter>
     </div>
   );
 }
