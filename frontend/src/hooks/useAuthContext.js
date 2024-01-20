36a21e97620f9547bdd1041826f9a1e454664669
IlYess <najiilyassoo@gmail.com>
2024-01-20 07:14:21 +0100

    Develop useAuthContext component

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,3 +1,12 @@
-export const useAuthContext = () => {
+import { AuthContext } from "../context/AuthContext"
+import { useContext } from "react"
 
+export const useAuthContext = () => {
+  const context = useContext(AuthContext)
+
+  if(!context) {
+    throw Error('useAuthContext must be used inside an AuthContextProvider')
+  }
+
+  return context
 }
