49fefcabd0b85468f2e4259abd49fe924d0d4da9
IlYess <najiilyassoo@gmail.com>
2024-01-22 06:52:59 +0100

    Setup useLogin hook

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,3 +1,36 @@
+import { useState } from "react"
+import { useAuthContext } from './useAuthContext'
+import axios from "axios";
+
 export const useLogin = () => {
+    const baseUrl = 'http://localhost:4000';
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+    const { dispatch } = useAuthContext()
     
+    
+    const login = async (email, password) => {
+        setIsLoading(true)
+        setError(null)
+
+        try{
+            const response = await axios.post(`${baseUrl}/api/users/login`, {
+                email,
+                password
+            })
+
+            const data = response.data;
+            
+            setIsLoading(false)
+
+            localStorage.setItem('user', JSON.stringify(data))
+
+            dispatch({type: 'LOGIN', payload: data})
+            setError(null)
+        }catch(error){
+            setError(error.response.data.error)
+            setIsLoading(false)
+        }
+    }
+    return { login, isLoading, error}
 }
