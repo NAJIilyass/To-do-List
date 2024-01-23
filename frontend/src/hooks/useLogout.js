d47b2d250f6e4d5e6df6c04225cd9c03b88eaf22
IlYess <najiilyassoo@gmail.com>
2024-01-23 18:13:13 +0100

    Setup useLogout hook VFinal

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,3 +1,18 @@
-export const useLogout = () => {
+import { useAuthContext } from "./useAuthContext"
+import { useTasksContext } from "./useTasksContext"
 
+export const useLogout = () => {
+    const { dispatch } = useAuthContext()
+    const { dispatch: dispatchTasks} = useTasksContext()
+
+    const logout = () => {
+        //Remove user from storage
+        localStorage.removeItem('user')
+
+        //dispatch logout action
+        dispatch({type: 'LOGOUT'})
+        dispatchTasks({ type: 'SET_TASKS', payload: null })
+    }
+
+    return {logout}
 }
