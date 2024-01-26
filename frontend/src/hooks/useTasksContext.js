deb7f73d510e5bfb99757f77a3620883117bb16b
IlYess <najiilyassoo@gmail.com>
2024-01-26 08:27:53 +0100

    Setup the useTasksContext hook

--- frontend/src/hooks/useTasksContext.js
+++ frontend/src/hooks/useTasksContext.js
@@ -1,3 +1,12 @@
-export const useTasksContext = () => {
+import { TasksContext } from "../context/TasksContext"
+import { useContext } from "react"
 
+export const useTasksContext = () => {
+  const context = useContext(TasksContext)
+
+  if(!context) {
+    throw Error('useTasksContext must be used inside an TasksContextProvider')
+  }
+
+  return context
 }
